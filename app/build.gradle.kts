ece4950c1071009859494054a722918cb8fbc29e
urlittledollstacy-web <urlittledollstacy@gmail.com>
2026-09-08 05:49:38 +0100

    Add Navigation Compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.apollo.runtime)
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
